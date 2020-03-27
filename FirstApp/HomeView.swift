bbfe7da63a5199741ec87a1913633b7b48aac3bb
sannanhafeez <sannanhafeez06@gmail.com>
2020-03-28 12:01:21 +1300

    Extract SectionView data to Section struct, and render SectionViews based on sectionData

--- FirstApp/HomeView.swift
+++ FirstApp/HomeView.swift
@@ -26,8 +26,8 @@ struct HomeView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
-                    ForEach(0 ..< 5) { item in
-                        SectionView()
+                    ForEach(sectionDate) { item in
+                        SectionView(section: item)
                     }
                 }
                 .padding(30)
@@ -46,23 +46,25 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype designs in SwiftUI")
+                Text(section.title)
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(Color.white)
                 
                 Spacer()
                 
-                Image("Logo1")
+                Image(section.logo)
             }
             
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -70,8 +72,23 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.5), radius: 20, x: 0, y: 20)
+        .shadow(color: section.color.opacity(0.5), radius: 20, x: 0, y: 20)
     }
 }
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionDate = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+    Section(title: "Introduction to Computer Networks", text: "12 Sections", logo: "Logo1", image: Image("Card2"), color: Color("card3")),
+    Section(title: "Basics of Human Computer Interaction", text: "20 Sections", logo: "Logo1", image: Image("Card4"), color: Color("card4"))
+]
