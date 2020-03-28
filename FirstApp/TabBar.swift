b3b3ac9f6d0fbfd24a115dde7fadabd055d70840
sannanhafeez <sannanhafeez06@gmail.com>
2020-03-28 16:40:39 +1300

    Add tab bar navigation, and change app entry point to it

---
+++ FirstApp/TabBar.swift
@@ -0,0 +1,31 @@
+//
+//  TabBar.swift
+//  FirstApp
+//
+//  Created by Sannan Hafeez on 28/03/20.
+//  Copyright © 2020 Sannan Hafeez. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
